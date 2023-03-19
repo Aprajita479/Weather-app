f2744a078febc10ee560e75cdaf48bb6408203cf
Aprajita Kumari <aprajitakumari479@gmail.com>
2023-03-19 21:11:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weatherApp';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  cityName?: string = 'Wellington';
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherdata(this.cityName as string);
+    this.cityName = '';
+  }
+  onSubmit() {
+    this.getWeatherdata(this.cityName as string);
+    this.cityName = '';
+  }
+  private getWeatherdata(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      },
+    });
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://openweatherapp.p.rapidapi.com/',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'openweatherapp.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '086f7615eamsh59dcd013447515fp1dabacjsn1b4c067175e1',
+};
