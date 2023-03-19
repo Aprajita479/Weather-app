05ab408515a5cbd5eb97709a10f20704c8ffecdc
Aprajita Kumari <67529260+Aprajita479@users.noreply.github.com>
2023-03-19 21:21:47 +0530

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  weatherApiBaseUrl: 'https://openweatherapp.p.rapidapi.com/',
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'openweatherapp.p.rapidapi.com',
-  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: '',
-};
